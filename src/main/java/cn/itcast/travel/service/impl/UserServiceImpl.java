d8c4665e6fbe3580fa23f3de2fa6d1918870da6d
wdz <buhtiglixingchen@gmail.com>
2021-06-09 19:12:35 +0800

    ^

---
+++ src/main/java/cn/itcast/travel/service/impl/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package cn.itcast.travel.service.impl;
+
+import cn.itcast.travel.dao.UserDao;
+import cn.itcast.travel.dao.impl.UserDaoImpl;
+import cn.itcast.travel.domain.User;
+import cn.itcast.travel.service.UserService;
+import cn.itcast.travel.util.MailUtils;
+import cn.itcast.travel.util.UuidUtil;
+
+public class UserServiceImpl implements UserService {
+    private UserDao userDao = new UserDaoImpl();
+
+    @Override
+    public boolean regist(User user) {
+        User u = userDao.findByUsername(user.getUsername());
+        if (null != u) {
+            return false;
+        }
+        user.setCode(UuidUtil.getUuid());
+        user.setStatus("N");
+        userDao.save(user);
+        String content = "<a href='http://localhost/travel/activeUserServlet?code=" + user.getCode() + "'>点击激活</a>";
+        MailUtils.sendMail(user.getEmail(), content, "激活travel账号");
+        return true;
+    }
+
+    @Override
+    public boolean active(String code) {
+        User user = userDao.findByCode(code);
+        if (user != null) {
+            userDao.updateStatus(user);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
